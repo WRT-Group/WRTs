be6bf3275fa502a9b4aeee5bc6abcf51ee8d5c7c
RayenTellissy <rayentellissy571@gmail.com>
2023-05-09 18:53:24 +0100

    updated model name in backend

--- backend/controller/NFT.js
+++ backend/controller/NFT.js
@@ -1,15 +1,15 @@
-const model=require('../model/NFT.js')
+const NFT=require('../model/NFT.js')
 
 const getAll= async(req,res)=>{
-    const data=await model.find({}).lean();
+    const data=await NFT.find({}).lean();
     res.json(data)
 }
 const addNFTs=async(req,res)=>{
-    await model.insertMany(req.body)
+    await NFT.insertMany(req.body)
     res.json('created')
 }
 const edit=async(req,res)=>{
-    await model.updateOne({_id:req.params.id},{nftName:req.body.nftName,price:req.body.price,image:req.body.image,description:req.body.description,owner:req.body.owner})
+    await NFT.updateOne({_id:req.params.id},{nftName:req.body.nftName,price:req.body.price,image:req.body.image,description:req.body.description,owner:req.body.owner})
     res.json('updated')
 }
 module.exports={
