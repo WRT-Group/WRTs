5b8cbd7e6ce96ba886c53dbddd5bee3c30166f8c
Wael Dghim <104037074+Wael-Dghim@users.noreply.github.com>
2023-05-09 17:12:46 +0100

    post-merge fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,29 +1,14 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import App from "./App.jsx";
-import "./index.css";
-=======
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom"
->>>>>>> bf76bcb779cd5308e75cdb77a66f9ec7e7121c63
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-<<<<<<< HEAD
-    <App />
-  </React.StrictMode>
-);
-=======
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
->>>>>>> bf76bcb779cd5308e75cdb77a66f9ec7e7121c63
+  </React.StrictMode>
+);
