a145960488764436f44f2512529f2f008f926298
RayenTellissy <rayentellissy571@gmail.com>
2023-05-09 16:43:18 +0100

    set up navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home/Home'
 import './App.css'
 
+import Signup from "./components/Auth/Signup/Signup"
+import Login from './components/Auth/Login/Login'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      yo
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
     </>
   )
 }
--- frontend/src/components/Auth/Signup/Signup.jsx
+++ frontend/src/components/Auth/Signup/Signup.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import "./Signup.css"
+
 const Signup = () => {
   return (
     <div>
-      
+
     </div>
   );
 };
