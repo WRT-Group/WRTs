3bee789a3a434d3aa8c22fe962abe168c0459d78
Wael Dghim <104037074+Wael-Dghim@users.noreply.github.com>
2023-05-09 15:27:49 +0100

    signup page functional, missing style & checks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
+import Signup from "./components/Auth/Signup/Signup";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
-      yo
-    </>
-  )
+  return <Signup />;
 }
 
-export default App
+export default App;
--- frontend/src/components/Auth/Signup/Signup.jsx
+++ frontend/src/components/Auth/Signup/Signup.jsx
@@ -1,10 +1,124 @@
-import React from 'react';
-import "./Signup.css"
+import React, { useState } from "react";
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBRow,
+  MDBCol,
+  MDBInput,
+} from "mdb-react-ui-kit";
+import axios from "axios";
+
+import "./Signup.css";
+
 const Signup = () => {
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = () => {
+    const newUser = {
+      fName,
+      lName,
+      username,
+      password,
+      confPassword,
+      email,
+    };
+
+    axios.post("http://localhost:3001/user/signup", newUser, {
+      headers: { "Content-Type": "application/json" },
+    });
+  };
   return (
-    <div>
-      
-    </div>
+    <MDBContainer fluid>
+      <MDBRow className="d-flex justify-content-center align-items-center">
+        <MDBCol lg="8">
+          <MDBCard
+            className="my-2 rounded-3 signup"
+            style={{ maxWidth: "600px" }}
+          >
+            <MDBCardImage
+              src="../../../../assets/istockphoto-1298834196-170667a.jpg"
+              className="w-100 rounded-top"
+              alt="Sample photo"
+            />
+
+            <MDBCardBody className="px-5">
+              <h3 className="mb-3 pb-md-0 px-md-2">Registration Info</h3>
+
+              <MDBRow>
+                <MDBCol md="6" className="py-0">
+                  <MDBInput
+                    onChange={(e) => setFName(e.target.value)}
+                    wrapperClass="name mb-2"
+                    label="First Name"
+                    id="form2"
+                    type="text"
+                  />
+                </MDBCol>
+
+                <MDBCol md="6" className="py-0">
+                  <MDBInput
+                    onChange={(e) => setLName(e.target.value)}
+                    wrapperClass="mb-2"
+                    label="Last Name"
+                    id="form3"
+                    type="text"
+                  />
+                </MDBCol>
+              </MDBRow>
+
+              <MDBInput
+                onChange={(e) => setUsername(e.target.value)}
+                wrapperClass="mb-2"
+                label="Username"
+                id="form1"
+                type="text"
+              />
+
+              <MDBInput
+                onChange={(e) => setEmail(e.target.value)}
+                wrapperClass="mb-2"
+                label="Email"
+                id="form3"
+                type="text"
+              />
+
+              <MDBInput
+                onChange={(e) => setPassword(e.target.value)}
+                wrapperClass="mb-2"
+                label="Password"
+                id="form1"
+                type="text"
+              />
+
+              <MDBInput
+                onChange={(e) => setConfPassword(e.target.value)}
+                wrapperClass="mb-2"
+                label="Confirm Password"
+                id="form1"
+                type="text"
+              />
+
+              <MDBBtn
+                color="success"
+                className="mb-2"
+                size="lg"
+                onClick={handleSubmit}
+              >
+                Submit
+              </MDBBtn>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 };
 
