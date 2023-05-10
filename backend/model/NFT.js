3696488d4e5109eb4e152668b1b2b874bbeb1aa3
Wael Dghim <104037074+Wael-Dghim@users.noreply.github.com>
2023-05-10 15:02:51 +0100

    model updates

--- backend/model/NFT.js
+++ backend/model/NFT.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const schema=mongoose.Schema({
+const schema = mongoose.Schema(
+  {
     nftName: String,
     price: Number,
     owner: String,
     image: String,
-    description: String
-})
+    description: String,
+  },
+  { timestamps: true }
+);
 
-const NFT=mongoose.model("NFT",schema)
+const NFT = mongoose.model("NFT", schema);
 
-module.exports=NFT
+module.exports = NFT;
