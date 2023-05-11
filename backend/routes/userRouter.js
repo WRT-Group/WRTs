929f1503449732b217abdb2776f123b0f4df6820
Wael Dghim <104037074+Wael-Dghim@users.noreply.github.com>
2023-05-11 13:54:57 +0100

    key prop added to div

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -17,6 +17,6 @@ router.get("/owner/:id", getUserByOwner);
 router.post("/signup", signup);
 router.post("/login", login);
 
-router.put("/:id", authenticate, update);
+router.put("/:id", update);
 
 module.exports = router;
